a20862b8ad666f9e60f046843d1499ecd8bf17a0
Jason Shaw <53303655+shawsers@users.noreply.github.com>
2021-11-01 13:58:31 -0400

    Update change_interval.sh

--- change_interval.sh
+++ change_interval.sh
@@ -8,9 +8,10 @@ echo "Starting script"
 # 5 - Scroll through the results and note the UUIDs for the vCenter category: “hypervisor” targets.
 #Now enter IP of your Topology Processor internal IP below and the UUID's of the targets above
 #This will query to see what the existing discovery internal is
+TPIP=$(kubectl get services | grep topo | awk '{print $3}')
 echo "Checking discovery intervals"
-curl -X POST "http://10.233.6.57:8080/ScheduleService/getDiscoverySchedule" -H "accept: application/json;charset=UTF-8" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"targetId\": 74054410720384}"
-curl -X POST "http://10.233.6.57:8080/ScheduleService/getDiscoverySchedule" -H "accept: application/json;charset=UTF-8" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"targetId\": 74054411682144}"
+curl -X POST "http://$TPIP:8080/ScheduleService/getDiscoverySchedule" -H "accept: application/json;charset=UTF-8" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"targetId\": 74054410720384}"
+curl -X POST "http://$TPIP:8080/ScheduleService/getDiscoverySchedule" -H "accept: application/json;charset=UTF-8" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"targetId\": 74054411682144}"
 #This will change the discovery interval as needed - change values in mins and seconds as you like
 #echo "Updating discovery intervals"
 #curl -X POST "http://10.233.6.57:8080/ScheduleService/setDiscoverySchedule" -H "accept: application/json;charset=UTF-8" -H "Content-Type: application/json;charset=UTF-8" -d "{ \"targetId\": 74054410720384, \"fullIntervalMinutes\": 10, \"incrementalIntervalSeconds\": 30}"

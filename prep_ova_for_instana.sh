2a22c52c5c3e54b642454bbda9c8da88f99cafc7
Jason Shaw <53303655+shawsers@users.noreply.github.com>
2023-03-01 05:24:26 -0500

    Update prep_ova_for_instana.sh

--- prep_ova_for_instana.sh
+++ prep_ova_for_instana.sh
@@ -9,12 +9,21 @@ lvchange -an /dev/mapper/turbo-var_lib_mysql
 lvremove /dev/mapper/turbo-var_lib_mysql
 lvmdiskscan
 pvscan
-echo "create new volume for Instana"
-lvcreate --name instana_data -l 100%FREE turbo
+echo "create new volumes for Instana"
+lvcreate --name instana_data -L 250G turbo -y
+lvcreate --name instana_metrics -L 250G turbo -y
+lvcreate --name instana_traces -L 250G turbo -y
 mkfs.ext4 /dev/turbo/instana_data
-mkdir /mnt
-mount /dev/turbo/instana_data /mnt
+mkfs.ext4 /dev/turbo/instana_metrics
+mkfs.ext4 /dev/turbo/instana_traces
 mkdir -p /mnt/data /mnt/metrics /mnt/traces
+mount /dev/turbo/instana_data /mnt/data
+mount /dev/turbo/instana_metrics /mnt/metrics
+mount /dev/turbo/instana_traces /mnt/traces
+echo "write new volumes to fstab"
+echo "/dev/mapper/turbo-instana_data	/mnt/data	ext4	defaults	0 0" >> /etc/fstab
+echo "/dev/mapper/turbo-instana_metrics	/mnt/metrics	ext4	defaults	0 0" >> /etc/fstab
+echo "/dev/mapper/turbo-instana_traces	/mnt/traces	ext4	defaults	0 0" >> /etc/fstab
 echo "install Instana"
 cat >/etc/yum.repos.d/Instana-Product.repo <<EOF
 [instana-product]
@@ -33,6 +42,7 @@ EOF
 yum clean expire-cache -y
 yum makecache -y
 yum install -y instana-console
-echo "**check for errors above and correct/rerun as needed**"
-echo "**update /etc/hosts with correct IP for the host**"
+echo "1**check for errors above and correct/rerun as needed**"
+echo "2**update /etc/hosts with correct IP for the host**"
+echo "3**remove old entry /var/lib_mysql from /etc/fstab"
 echo "Instana install prep done, proceed with instana init"

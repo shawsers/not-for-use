9e1ce066fa3431ccc7530b019ebaae4e1c5c8989
Jason Shaw <53303655+shawsers@users.noreply.github.com>
2026-09-09 09:58:17 -0400

    Create broadcast.sh

---
+++ broadcast.sh
@@ -0,0 +1,7 @@
+#Turbo Broadcast in k8s
+echo ###########################
+echo # RUNNING TURBO BROADCAST #
+ECHO ###########################
+rs=$(kubectl get pod -n turbonomic | grep rsyslog | awk '{print $1}')
+tp=$(kubectl get services -n turbonomic | grep topology-processor | grep 8080 | awk '{print $3}')
+kubectl exec -it $rs -n turbonomic -- curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' http://$tp:8080/topology/send
